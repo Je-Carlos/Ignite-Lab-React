33e1cdc078ac12be604a4788ae718a51248dca0a
Jean <cpjeanberg@gmail.com>
2022-06-23 12:56:29 -0300

    Criação do menu de vídeos + renderização de vídeos

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
 import { useEffect, useState } from "react";
+import { ApolloProvider } from "@apollo/client";
 import logo from "./logo.svg";
 import "./App.css";
 import "../src/styles/global.css";
 import { gql, useQuery } from "@apollo/client";
 import { client } from "./lib/apollo";
-
-//Fazer o query para pegar as aulas e professor através do Apollo
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-        bio
-      }
-    }
-  }
-`;
-
-// Criando a interface para definir os tipos de Lesson
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Header } from "./components/Header";
+import { Event } from "./pages/Event";
+import { Router } from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY); // Utilizando o Hook useQuery para fazer o query
-  console.log(data);
-
-  /* Metodo sem utilizar Hooks
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, []) */
-
   return (
-    <ul>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { ApolloProvider } from "@apollo/client";
+
 import { client } from "./lib/apollo";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,8 +7,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 );
